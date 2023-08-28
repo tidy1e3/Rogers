7766221d6ccfc1401afa366c44a56b6086c614d6
tidy1e3 <143417070+tidy1e3@users.noreply.github.com>
2023-08-28 06:59:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_gcsz408", "template_y8hxmbj", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
